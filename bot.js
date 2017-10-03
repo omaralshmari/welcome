6e0a0d303e14eb2942c021359dbfbde24dc1ca9f
shibegod <31591898+shibegod@users.noreply.github.com>
2017-10-02 22:35:24 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+ const Discord = require("discord.js");
+ 
+ var bot = new Discord.Client();
+ 
+ bot.on("ready", () => {
+     console.log("Ready");
+  });
+  
+  bot.on('guildMemberAdd',  (member) => {
+ -    member.guild.channels.get("343037231734456322").send("Welcome to the greatest discord server of all time!, " + member);
+ +    member.guild.channels.get("343037231734456322").send("Hey dumbass welcome to this toxic cancerous gay fucking discord server hope you don't have fun bitch. Eat a dick " + member);
+      });
+      
+      bot.login(process.env.BOT_TOKEN); 
